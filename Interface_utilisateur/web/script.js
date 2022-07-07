80f5cc9ddc0bd5e909a73a636a9af97ac0759a71
Gavau <alexandregavaudan0@gmail.com>
2022-07-08 01:02:16 +0200

    Interface 2

---
+++ Interface_utilisateur/web/script.js
@@ -0,0 +1,41 @@
+
+
+document.querySelector("#bird").onclick = function () {
+    eel.source_bird()(function () {
+    })
+}
+
+document.querySelector("#shark").onclick = function () {
+    eel.source_shark()(function () {
+    })
+}
+
+document.querySelector("#objets").onclick = function () {
+    eel.source_objets()(function () {
+    })
+}
+
+document.querySelector("#statue").onclick = function () {
+    eel.source_statue()(function () {
+    })
+}
+
+document.querySelector("#forest").onclick = function () {
+    eel.target_forest()(function () {
+    })
+}
+
+document.querySelector("#ocean").onclick = function () {
+    eel.target_ocean()(function () {
+    })
+}
+
+document.querySelector("#sky").onclick = function () {
+    eel.target_sky()(function () {
+    })
+}
+
+document.querySelector("#run").onclick = function () {
+    eel.run()(function () {
+    })
+}
